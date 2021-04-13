2d7979538900e4bfa859563bd7b733e47e31c1ce
tsn3 <tsn3ps@gmail.com>
2021-04-13 16:09:50 +0300

    show csv

--- mvc/views/catalog/index.php
+++ mvc/views/catalog/index.php
@@ -1,24 +1,21 @@
 <?php require_once $_SERVER ['DOCUMENT_ROOT'] . '/views/header_second.php';?>
 <?php
-$csvData = file_get_contents("products.csv");
-$lines = explode(PHP_EOL, $csvData);
-$array = array();
-foreach ($lines as $line) {
-    $array[] = str_getcsv($line);
-//    return $line;
-}
-//echo '<pre>';
-//print_r($array);
-//echo '</pre>';
-//echo 'View Catalog <br/>';
-?>
+$data = File("products.csv");
+echo "<b><i><h2><center>Прайс-лист</b></i></h2></center>";
+echo "<center><table><tr>";
+$dat_arr = explode(";", $data[0]);
 
-        <ul>
-            <?php foreach($lines as $line){
-                $array[] = str_getcsv($line);
-                echo $line?>
-                <li><span><?php echo $line['title'] . ' - ' . $line['price'] . '$';?></span></li>
-            <?php } ?>
-        </ul>
+for ($p=0;$p<count($dat_arr);$p++) {
+    echo "<td ><center><b><i>$dat_arr[$p]";
+}
+echo "</tr>";
+for ($i=1;$i<count($data);$i++) {
+    $data_array = explode(";", $data[$i]);
+    echo "<tr>";
+    for ($f=0;$f<count($data_array);$f++) {
+        echo "<td><center><b><i>$data_array[$f]";
+    }
+    echo "</tr>";}echo "</table></center>";
+?>
 
 <?php require_once $_SERVER ['DOCUMENT_ROOT'] . '/views/footer.php';?>
